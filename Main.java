1d0bd640fe536e25d9904c233f2ea0c30a4779b8
heshancampus1028-dev <heshancampus1028@gmail.com>
2026-05-04 20:23:52 +0530

    Implemented viewStudents feature

--- Main.java
+++ Main.java
@@ -5,5 +5,20 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Student Record Manager");
+        addStudent("Alice");
+        addStudent("Bob");
+        viewStudents();
+    }
+
+    static void addStudent(String name) {
+        students.add(name);
+        System.out.println(name + " added.");
+    }
+
+    static void viewStudents() {
+        System.out.println("Student List:");
+        for (String s : students) {
+            System.out.println(s);
+        }
     }
 }
